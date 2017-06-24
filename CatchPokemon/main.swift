e6340d910fce22d28d0caa496a3d2ab3fa456d6e
Matt Ruston <matt@rustonmail.com>
2017-06-24 10:32:31 -0500

    Initial Commit

---
+++ CatchPokemon/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CatchPokemon
+//
+//  Created by Matthew Ruston on 6/24/17.
+//  Copyright © 2017 MattRuston. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
